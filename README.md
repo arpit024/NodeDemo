6efc1c17559377deae62503698cb3e73ca81a84f
Arpit <kunal.alld@gmail.com>
2020-11-28 20:56:07 +0530

    db connect check

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NodeDemo
+# make sure mysql server is running
 # run DB_script file
 # update mysql username and password in .env file
 # npm install
--- config/db.js
+++ config/db.js
@@ -3,9 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports =  new Sequelize(process.env.DB_NAME,
+const sequelize =  new Sequelize(process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
 });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  module.exports=sequelize;
